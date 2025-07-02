b60393d3fb846521ddd5d89183b50e8566bca9e1
HP_pellel <algassimoupelleldiallo2005@gmail.com>
2025-07-02 14:42:53 +0200

    V1.1

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('BASE_URL', '/~dialloa/projet-solo/CCFG-667/');
-define('CSS_PATH', BASE_URL . 'css/styles.css');
-define('JS_PATH', BASE_URL . 'js/script.js');
+define('BASE_URL', '/~dialloa/projet-solo/CCFG-667');
+define('CSS_PATH', BASE_URL . '/css/styles.css');
+define('JS_PATH', BASE_URL . '/js/script.js');
