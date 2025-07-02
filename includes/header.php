063e7f46a54ce7252380ebcd5e205dc05f4a0603
HP_pellel <algassimoupelleldiallo2005@gmail.com>
2025-07-02 23:37:54 +0200

    V2.1.1

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <?php
-require_once './config.php';
-require_once './counter.php';
+require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/counter.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
